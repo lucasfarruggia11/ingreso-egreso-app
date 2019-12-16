d361885ca6732cf5828eacc7bc65e635a9f04cf5
Lucas Farruggia <lucas.farruggia11@gmail.com>
2019-12-16 15:15:31 -0300

    first commit

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-// import { AppState } from '../../app.reducer';
 import { Subscription } from 'rxjs';
 import { IngresoEgreso } from '../ingreso-egreso.model';
+
+
+
 import { List } from 'linqts';
 
 
 import * as fromIngresoEgreso from '../ingreso-egreso.reducer';
-import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-estadistica',
-  templateUrl: './estadistica.component.html',
-  styles: [],
-  providers: [DatePipe]
+  templateUrl: './estadistica.component.html'
 })
-  
+
 export class EstadisticaComponent implements OnInit {
 
   public barChartOptions: any = {
@@ -23,13 +23,15 @@ export class EstadisticaComponent implements OnInit {
     responsive: true
   };
 
-  public barChartLabels: String[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartLabels: String[] = ['Enero', 'Febrero', 'Marzo', 'Abril',
+    'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+    'Octubre', 'Noviembre', 'Diciembre'];
   public barChartType: String = 'bar';
   public barChartLegend: Boolean = true;
 
   public barChartData: any[] = [
-    {data: [1, 2, 2, 1], label: 'Ingresos'},
-    {data: [0, 1, 1, 1], label: 'Egresos'}
+    {data: [], label: 'Ingresos'},
+    {data: [], label: 'Egresos'}
   ];
 
   ingresos: number;
@@ -43,7 +45,9 @@ export class EstadisticaComponent implements OnInit {
   public doughnutChartLabels: string[] = ['Ingresos', 'Egresos'];
   public doughnutChartData: number[] = [];
 
-  constructor( private store: Store<fromIngresoEgreso.AppState>, private datePipe: DatePipe) { }
+  
+
+  constructor(private store: Store<fromIngresoEgreso.AppState>) {}
 
   ngOnInit() {
     this.subscription = this.store.select('ingresoEgreso')
@@ -51,17 +55,14 @@ export class EstadisticaComponent implements OnInit {
               this.contarIngresoEgreso( ingresoEgreso.items );
             });
   }
-
   // events
   public chartClicked(e: any): void {
-    console.log(e);
   }
 
   public chartHovered(e: any): void {
-    console.log(e);
   }
 
-  contarIngresoEgreso( items: IngresoEgreso[] ) {
+  contarIngresoEgreso(items: IngresoEgreso[]) {
 
     this.ingresos = 0;
     this.egresos = 0;
@@ -71,49 +72,23 @@ export class EstadisticaComponent implements OnInit {
 
     const trasnsacciones = new List<IngresoEgreso>();
 
-    const hoy = new Date();
-    hoy.setMinutes(hoy.getMinutes() + hoy.getTimezoneOffset());
-    const start = new Date(hoy.getFullYear(), hoy.getMonth());
-    const end = new Date(hoy.getFullYear(), hoy.getMonth() + 1);
-
     this.cuantosEgresos = trasnsacciones.Where(item => item.tipo === 'ingreso').Count();
     this.ingresos = trasnsacciones.Where(item => item.tipo === 'ingreso').Sum(x => x.monto);
 
     items.forEach(item => {
       trasnsacciones.Add(item);
-      // if (item.fechaPago > start && item.fechaPago < end) {
-      //   if ( item.tipo === 'ingreso' ) {
-      //     this.cuantosIngresos ++;
-      //     this.ingresos += item.monto;
-      //   } else {
-      //     this.cuantosEgresos ++;
-      //     this.egresos += item.monto;
-      //   }
-      // }
     });
 
-    const transaccionesPorMes = trasnsacciones.GroupBy(item => this.datePipe.transform(item.fechaPago, 'yyyy/MM'));
-
-    if (transaccionesPorMes) {
-      this.barChartLabels = Object.keys(transaccionesPorMes);
-      // this.barChartData = Object.values(transaccionesPorMes);
-      // console.log(Object.keys(transaccionesPorMes));     // > ['1', '4', '8']
-      // console.log(Object.values(transaccionesPorMes));  // > [['Whiskers'], ['Boots', 'Daisy'], ['Barley']]
-      // console.log(Object.entries(transaccionesPorMes));
-
-      console.log(this.barChartLabels[0]);
-
-      // for (let [key, value] of Object.entries(transaccionesPorMes)) {
-      //   console.log(key + ' ' + value); // "a 5", "b 7", "c 9"
-      //   value.forEach(element => {
-      //     console.log(element);
-      //   });
-      // }
-    }
-
-    // transaccionesPorMes.array.forEach(element => {
-    //   console.log(element);
-    // });
+    this.barChartLabels.forEach((item, index) => {
+      console.log(`item:${item} index:${index}`);
+      const ingresos = trasnsacciones.Where(x => x.fechaPago.getFullYear() === 2019 &&
+                                            x.fechaPago.getMonth() === index && x.tipo === 'ingreso').Sum(x => x.monto);
+      const egresos = trasnsacciones.Where(x => x.fechaPago.getFullYear() === 2019 &&
+        x.fechaPago.getMonth() === index && x.tipo === 'egreso').Sum(x => x.monto);
+        console.log(`ingresos:${ingresos} egresos:${egresos}`);
+      this.barChartData[0].data[index] = ingresos;
+      this.barChartData[1].data[index] = egresos;
+    });
 
     this.cuantosIngresos = trasnsacciones.Where(item => item.tipo === 'ingreso').Count();
     this.ingresos = trasnsacciones.Where(item => item.tipo === 'ingreso').Sum(x => x.monto);
--- src/app/ingreso-egreso/ingreso-egreso.module.ts
+++ src/app/ingreso-egreso/ingreso-egreso.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { IngresoEgresoComponent } from './ingreso-egreso.component';
 import { EstadisticaComponent } from './estadistica/estadistica.component';
 import { DetalleComponent } from './detalle/detalle.component';
 import { OrdenIngresoEgresoPipe } from './orden-ingreso-egreso.pipe';
+
+
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts';
